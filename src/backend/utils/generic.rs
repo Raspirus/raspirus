6379210023dc3f192a7c5fe4ceb9d5a89d61cfd7
GamingGuy003 <bjornfelixh@gmail.com>
2024-10-24 22:22:41 +0200

    Integrated new pdf generation

--- src/backend/utils/generic.rs
+++ src/backend/utils/generic.rs
@@ -1,12 +1,11 @@
 use std::{
     fs::{self, File},
-    io::{BufRead, BufReader, BufWriter, Read, Write},
+    io::{BufReader, Read, Write},
     path::{Path, PathBuf},
 };
 
 use chrono::Local;
 use log::{debug, info, warn};
-use printpdf::PdfDocument;
 use sha2::{Digest, Sha256};
 use yara_x::Rules;
 use zip::write::SimpleFileOptions;
@@ -188,115 +187,6 @@ pub fn update_config(value: ConfigValue) -> Result<Config, String> {
     }
     config.save()?;
     Ok(config.clone())
-}
-
-static PAGE_HEIGHT: f32 = 297.0;
-static PAGE_WIDTH: f32 = 210.0;
-static FONT_SIZE: f32 = 7.0;
-
-pub fn create_pdf(log_file: PathBuf) -> Result<PathBuf, String> {
-    // fetch filename from log path
-    let file_name = log_file.file_name().unwrap_or_default().to_string_lossy();
-    let timestamp = file_name.trim_end_matches(".log");
-    // fetch downloads directory
-    let downloads_folder = crate::CONFIG
-        .lock()
-        .map_err(|err| format!("Failed to lock config: {err}"))?
-        .paths
-        .clone()
-        .ok_or_else(|| "No paths?".to_string())?
-        .downloads;
-    info!("Creating pdf report for {}", log_file.to_string_lossy());
-
-    let log =
-        File::open(log_file.clone()).map_err(|err| format!("Failed to open logfile: {err}"))?;
-
-    // create pdf file
-    let (doc, page1, layer1) = PdfDocument::new(
-        format!("Log {timestamp}",),
-        printpdf::Mm(PAGE_WIDTH),
-        printpdf::Mm(PAGE_HEIGHT),
-        "Layer 1",
-    );
-
-    // set font
-    let font = doc
-        .add_builtin_font(printpdf::BuiltinFont::Helvetica)
-        .map_err(|err| format!("Failed to add font: {err}"))?;
-    let mut current_layer = doc.get_page(page1).get_layer(layer1);
-
-    current_layer.begin_text_section();
-    current_layer.set_font(&font, FONT_SIZE);
-
-    current_layer.set_text_cursor(printpdf::Mm(10.0), printpdf::Mm(PAGE_HEIGHT - 10.0));
-    current_layer.set_line_height(FONT_SIZE + 2.0);
-
-    current_layer.write_text(format!("Log {timestamp}"), &font);
-    current_layer.add_line_break();
-    current_layer.add_line_break();
-
-    // weird code with magic numbers but what can you do /shrug
-    let pt_in_mm = (FONT_SIZE + 2.0) * 0.3537778;
-    let max_lines_page = ((PAGE_HEIGHT - 10.0) / pt_in_mm) as usize;
-    let max_chars_per_line = (PAGE_WIDTH - 10.0) / (FONT_SIZE * 0.5 * 0.3537778);
-    debug!("Determined lines per page should be {max_lines_page}, and max chars per line should be {max_chars_per_line}");
-
-    // title + empty line
-    let mut current_line = 1;
-
-    for line in std::io::BufReader::new(log).lines() {
-        // if we reach the maximum lines per page we create a new one
-        if current_line % max_lines_page == 0 && current_line > 0 {
-            // cleanup old page
-            current_layer.end_text_section();
-
-            debug!("Page end reached, creating new page at line {current_line}");
-            let (page, layer) = doc.add_page(
-                printpdf::Mm(PAGE_WIDTH),
-                printpdf::Mm(PAGE_HEIGHT),
-                "Layer 1",
-            );
-
-            // prepare new page
-            current_layer = doc.get_page(page).get_layer(layer);
-            current_layer.begin_text_section();
-            current_layer.set_font(&font, FONT_SIZE);
-
-            current_layer.set_text_cursor(printpdf::Mm(10.0), printpdf::Mm(PAGE_HEIGHT - 10.0));
-            current_layer.set_line_height(FONT_SIZE + 2.0);
-        }
-
-        // write the current line to pdf
-        match line {
-            Ok(line) => {
-                let mut lines = Vec::new();
-
-                // split line into pieces that fit on the page
-                lines.extend(
-                    line.chars()
-                        .collect::<Vec<char>>()
-                        .chunks(max_chars_per_line as usize)
-                        .map(|chars| chars.iter().collect())
-                        .collect::<Vec<String>>(),
-                );
-
-                for line in lines {
-                    current_layer.write_text(line, &font);
-                    current_layer.add_line_break();
-                    current_line += 1;
-                }
-            }
-            Err(err) => return Err(format!("Could not read log file line: {err}")),
-        }
-    }
-    current_layer.end_text_section();
-
-    doc.save(&mut BufWriter::new(
-        File::create(downloads_folder.join(format!("{timestamp}.pdf")))
-            .map_err(|err| format!("Failed to open file for writing: {err}"))?,
-    ))
-    .map_err(|err| format!("Failed to save pdf: {err}"))?;
-    Ok(downloads_folder.join(format!("{timestamp}.pdf")))
 }
 
 pub fn download_logs() -> Result<PathBuf, String> {
--- src/backend/utils/mod.rs
+++ src/backend/utils/mod.rs
@@ -1,4 +1,6 @@
 /// generic functions intented for reuse
 pub mod generic;
+/// pdf report generator
+pub mod pdf_gen;
 /// various usb related functions
 pub mod usb_utils;
