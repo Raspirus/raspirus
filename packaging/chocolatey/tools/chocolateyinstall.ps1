82422d903d537fb1c36b77d3071687d5539788f2
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2025-01-22 17:58:04 +0100

    Added packaging

---
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/Raspirus/raspirus/releases/download/v2.1.0/raspirus_2.1.0_x64_en-US.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url64bit      = $url64
+  softwareName  = 'raspirus'
+  checksum64    = 'bf37bf8b199c8ad4765f792506dcf496091f3ccf8e0ca1154e9223ae23241ec1'
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
