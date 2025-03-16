753537d4d52d4794cfd57f25413b9e335de745a2
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-16 20:35:31 +0100

    Reimplemented config handling

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@
 
 use relm4::RelmApp;
 
+mod backend;
 mod frontend;
-mod tests;
+mod globals;
 
 fn main() -> Result<(), String> {
+    //let time = chrono::NaiveDateTime::parse_from_str("2024-09-20T19:50:20Z", "%Y-%m-%dT%H:%M:%SZ");
+    //dbg!(time);
+
     let app = RelmApp::new("raspirus.app");
     app.run::<frontend::main::model::AppModel>(0);
     Ok(())
