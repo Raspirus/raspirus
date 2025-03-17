6ef397ad8a96cc2718b1e508ab6e3ea1a9012c4f
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-17 14:54:05 +0100

    Added blanket for Log

--- src/backend/log.rs
+++ src/backend/log.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::{path::PathBuf, fs};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum LogLevel {
@@ -6,4 +7,24 @@ pub enum LogLevel {
     Info,
     Debug,
     Trace
+}
+
+/// A helper used for logging file scan results to a logfile
+pub struct Log {
+    pub log_path: PathBuf,
+}
+
+impl Log {
+    /// Tries to create a new instance of Log, while also creating the log file if it does not
+    /// already exist
+    pub fn new(log_path: PathBuf) -> Result<Self, String> {
+        if !log_path.exists() {
+            fs::create_dir_all(&log_path).map_err(|err| format!("Failed to create log file: {err:?}"))?;
+        }
+        Ok(Self { log_path })
+    }
+
+    pub fn log() -> Result<(), String> {
+        todo!()
+    }
 }
