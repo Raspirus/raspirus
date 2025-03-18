b1802e5f4c07444ebccd3bbc2333618b1c057561
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-18 19:02:50 +0100

    Rewrote file indexing
    Former-commit-id: 6e5df5eb1c68d701124d7dc4dd3098bea11b03f0
    Former-commit-id: 9627358a86b036c4866b624ef1a129102eb463c5

---
+++ src/backend/scanner.rs
@@ -0,0 +1,89 @@
+use std::path::PathBuf;
+
+use log::{trace, warn};
+
+struct Index {
+    pub paths: Vec<PathBuf>,
+}
+
+impl Index {
+    fn new(root: PathBuf) -> Result<Self, String> {
+        let mut indexed = Self { paths: Vec::new() };
+        if root.is_dir() {
+            indexed.index_folder(&root);
+        }
+
+        if root.is_file() {
+            indexed.index_file(&root)?;
+        }
+
+        Ok(indexed)
+    }
+
+    /// Tries to add a file path to the path list
+    fn index_file(&mut self, root: &PathBuf) -> Result<(), String> {
+        // checks if file exists
+        if !root.exists() {
+            Err(format!("File {} does not exist", root.display()))?
+        }
+
+        // checks if permissions suffice, and if file is symlink
+        if root
+            .metadata()
+            .map_err(|err| format!("Failed to get metadata: {err:?}"))?
+            .is_symlink()
+        {
+            Err(format!("File {} is a symlink", root.display()))?
+        }
+
+        Ok(self.paths.push(root.to_path_buf()))
+    }
+
+    /// Tries to add all of a folders subfolders / files to the list of paths
+    fn index_folder(&mut self, root: &PathBuf) -> Result<(), String> {
+        // go through all children of a folder
+        for entry in std::fs::read_dir(root).map_err(|err| {
+            format!(
+                "Failed to get folder entries for {}: {err:?}",
+                root.display()
+            )
+        })? {
+            // if we can fetch entry, save its path, otherwise log and skip
+            let root = match entry {
+                Ok(entry) => entry,
+                Err(err) => {
+                    warn!("Failed to get entry: {err:?}; Skipping...");
+                    continue;
+                }
+            }
+            .path();
+
+            // if entry is file, index it, otherwise index it as folder
+            if root.is_file() {
+                match self.index_file(&root) {
+                    Ok(_) => trace!("Indexed {}", root.display()),
+                    Err(err) => warn!("{err}; Skipping..."),
+                }
+            } else if !root.is_symlink() {
+                match self.index_folder(&root) {
+                    Ok(_) => trace!("Indexed {}", root.display()),
+                    Err(err) => warn!("{err}; Skipping..."),
+                }
+            } else {
+                warn!("{} is a symlink; Skipping...", root.display());
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct Scanner {}
+
+impl Scanner {
+    pub fn new(root: PathBuf) -> Result<Self, String> {
+        let indexed = Index::new(root)?;
+        dbg!(indexed.paths.len());
+        Ok(Self {})
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 //#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::PathBuf;
+
+use backend::scanner::{self, Scanner};
+use log::LevelFilter;
 use relm4::RelmApp;
+use simplelog::{Config, TermLogger};
 
 mod backend;
 mod frontend;
@@ -10,6 +15,13 @@ mod globals;
 fn main() -> Result<(), String> {
     //let time = chrono::NaiveDateTime::parse_from_str("2024-09-20T19:50:20Z", "%Y-%m-%dT%H:%M:%SZ");
     //dbg!(time);
+    
+    // init logger with possibly inserted loglevel
+    let log_level = std::env::var("RUST_LOG").unwrap_or("Info".to_owned());
+    let level_filter = log_level.parse::<LevelFilter>().unwrap_or(LevelFilter::Info);
+    TermLogger::init(level_filter, Config::default(), simplelog::TerminalMode::Mixed, simplelog::ColorChoice::Always).unwrap();
+
+    let scanner = Scanner::new(PathBuf::from("/home/gamingguy003/Games"))?;
 
     let app = RelmApp::new("raspirus.app");
     app.run::<frontend::main::model::AppModel>(0);
