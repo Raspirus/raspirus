0e2d33c0de71d8548f936e22cd175840bde18e75
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-18 23:00:18 +0100

    Removed dependency on lazy_static in favor of custom mut global handling

--- src/globals.rs
+++ src/globals.rs
@@ -1,11 +1,23 @@
-use lazy_static::lazy_static;
+use std::sync::{Arc, Mutex, OnceLock};
 
-use crate::backend::log::LogLevel;
+use crate::backend::{config::Config, log::LogLevel};
 
-// More intricate globals
-lazy_static! {
-    /// String with log init time
-    pub static ref APPLICATION_LOG: String = chrono::Local::now().format("%Y_%m_%d_%H_%M_%S").to_string();
+// global mutable values
+/// Application startup time used for logging
+pub static APPLICATION_LOG: OnceLock<String> = OnceLock::new();
+
+/// Returns the time that the application was booted up
+pub fn get_application_log() -> String {
+    APPLICATION_LOG.get_or_init(|| chrono::Utc::now().format("%Y-%m-%dT:%H:%M:%SZ").to_string()).to_string()
+}
+
+
+/// Shared config file
+pub static CONFIG: OnceLock<Arc<Mutex<Config>>> = OnceLock::new();
+
+/// Returns the shared config
+pub fn get_config() -> Arc<Mutex<Config>> {
+    CONFIG.get_or_init(|| Arc::new(Mutex::new(Config::default()))).clone()
 }
 
 // A bunch of default values
@@ -14,3 +26,5 @@ pub static DEFAULT_LOG_LEVEL: LogLevel = LogLevel::Debug;
 pub static DEFAULT_LANGUAGE: &str = "en_US";
 pub static CONFIG_VERSION: usize = 0;
 pub static CONFIG_FILE_NAME: &str = "raspirus.cfg";
+
+
