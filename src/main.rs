9879d6664ef0301ca527fb2d2072cce3aa04b862
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-18 19:03:42 +0100

    Removed unwrap

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,19 @@ mod globals;
 fn main() -> Result<(), String> {
     //let time = chrono::NaiveDateTime::parse_from_str("2024-09-20T19:50:20Z", "%Y-%m-%dT%H:%M:%SZ");
     //dbg!(time);
-    
+
     // init logger with possibly inserted loglevel
     let log_level = std::env::var("RUST_LOG").unwrap_or("Info".to_owned());
-    let level_filter = log_level.parse::<LevelFilter>().unwrap_or(LevelFilter::Info);
-    TermLogger::init(level_filter, Config::default(), simplelog::TerminalMode::Mixed, simplelog::ColorChoice::Always).unwrap();
+    let level_filter = log_level
+        .parse::<LevelFilter>()
+        .unwrap_or(LevelFilter::Info);
+    TermLogger::init(
+        level_filter,
+        Config::default(),
+        simplelog::TerminalMode::Mixed,
+        simplelog::ColorChoice::Always,
+    )
+    .map_err(|err| format!("Failed to initialize termlogger: {err:?}"))?;
 
     let scanner = Scanner::new(PathBuf::from("/home/gamingguy003/Games"))?;
 
