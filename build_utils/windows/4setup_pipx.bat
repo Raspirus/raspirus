e9c70a32914d9c48da3693766741f36347e1e118
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-20 23:10:51 +0100

    changed gvsbuild

--- build_utils/windows/4setup_pipx.bat
+++ build_utils/windows/4setup_pipx.bat
@@ -1,2 +1,2 @@
-python -m pip install --user pipx
-python -m pipx ensurepath
+%LOCALAPPDATA%\Programs\Python\Python313\python -m pip install --user pipx
+%LOCALAPPDATA%\Programs\Python\Python313\python -m pipx ensurepath
