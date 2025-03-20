8f7b9408f94ece1e7e4b1b330297d75f03557c24
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-20 22:02:55 +0100

    Rewrote downloader

--- build_utils/windows/1setup_winget.ps1
+++ build_utils/windows/1setup_winget.ps1
@@ -1,12 +1,11 @@
-if (-not ([System.IO.File]::Exists("%LOCALAPPDATA%\Microsoft\WindowsApps\winget.exe"))) {
 Add-AppxPackage -Path "build_utils\windows\xaml.appx"
 Add-AppxPackage -Path "build_utils\windows\vctools.appx"
-$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-$URL = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content | ConvertFrom-Json |
+if (-not ([System.IO.File]::Exists("build_utils\windows\winget.appx"))) {
+    $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    $URL = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content | ConvertFrom-Json |
         Select-Object -ExpandProperty "assets" |
         Where-Object "browser_download_url" -Match '.msixbundle' |
         Select-Object -ExpandProperty "browser_download_url"
-# Remove-Item "build_utils\windows\winget.appx"
-Invoke-WebRequest -Uri $URL -OutFile "build_utils\windows\winget.appx" -UseBasicParsing
-Add-AppxPackage -Path "build_utils\windows\winget.appx"
+    Invoke-WebRequest -Uri $URL -OutFile "build_utils\windows\winget.appx" -UseBasicParsing
 }
+Add-AppxPackage -Path "build_utils\windows\winget.appx"
