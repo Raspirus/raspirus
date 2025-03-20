df1b4bbfbd8b8746e197dfc64df636c19af7754c
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-20 22:36:37 +0100

    userprofile

--- build_utils/windows/6build_gtk.bat
+++ build_utils/windows/6build_gtk.bat
@@ -1,3 +1,3 @@
 mkdir C:\Python313
 for /f "delims=" %%i in ('where python') do copy "%%i" "C:\Python313\python.exe" /Y
-%USERDATA%\.local\bin\gvsbuild build gtk4
+%USERPROFILE%\.local\bin\gvsbuild build gtk4
