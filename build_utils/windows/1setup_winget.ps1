a4d55bd4bc1a077384f8720377a9df9e29dea237
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-21 01:03:03 +0100

    Rewrote winget setup

--- build_utils/windows/1setup_winget.ps1
+++ build_utils/windows/1setup_winget.ps1
@@ -1,11 +1,8 @@
-Add-AppxPackage -Path "build_utils\windows\xaml.appx"
-Add-AppxPackage -Path "build_utils\windows\vctools.appx"
-if (-not ([System.IO.File]::Exists("build_utils\windows\winget.appx"))) {
-    $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-    $URL = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content | ConvertFrom-Json |
-        Select-Object -ExpandProperty "assets" |
-        Where-Object "browser_download_url" -Match '.msixbundle' |
-        Select-Object -ExpandProperty "browser_download_url"
-    Invoke-WebRequest -Uri $URL -OutFile "build_utils\windows\winget.appx" -UseBasicParsing
-}
-Add-AppxPackage -Path "build_utils\windows\winget.appx"
+# install vclibs
+Add-AppxPackage 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+# install dependencies
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip -OutFile .\DesktopAppInstaller_Dependencies.zip
+Expand-Archive .\DesktopAppInstaller_Dependencies.zip
+Add-AppxPackage .\DesktopAppInstaller_Dependencies\x64\*
+# install winget
+Add-AppxPackage 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
