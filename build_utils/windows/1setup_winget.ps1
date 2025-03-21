118546a607e5a320254e70c6c06478a02afeb80d
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-21 02:45:32 +0100

    changed winget to install with license

--- build_utils/windows/1setup_winget.ps1
+++ build_utils/windows/1setup_winget.ps1
@@ -5,4 +5,6 @@ Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/d
 Expand-Archive .\DesktopAppInstaller_Dependencies.zip
 Add-AppxPackage .\DesktopAppInstaller_Dependencies\x64\*
 # install winget
-Add-AppxPackage 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/4df037184d634a28b13051a797a25a16_License1.xml' -OutFile winget_license.xml -UseBasicParsing
+Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile winget.appx -UseBasicParsing
+Add-AppxProvisionedPackage -Online -PackagePath winget.appx -LicensePath winget_license.xml
