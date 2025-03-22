d33d2c1fa02960e4c49770f9a0a43b6828256944
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-22 03:27:20 +0100

    that was unnecessary

--- build_utils/windows/4update_path.bat
+++ build_utils/windows/4update_path.bat
@@ -1,3 +1,1 @@
-setx Path "%PATH%;C:\msys64\mingw64\bin"
-setx Lib "C:\msys64\mingw64\lib"
-setx PKG_CONFIG_PATH "C:\msys64\mingw64\lib\pkgconfig"
+setx Path "%PATH%;C:\msys64\mingw64\include;C:\msys64\mingw64\bin;C:\msys64\mingw64\lib"
