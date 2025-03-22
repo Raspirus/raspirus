6fea2c796ea73a16a8a5ddf0f2beebf193c13326
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-22 23:27:13 +0100

    Started on help

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ fn main() -> Result<(), Error> {
         Err(Error::InvalidArgument(invalid))?
     }
 
+    if let Some(Argument::Help) = get_argument(&Argument::Help) {}
+
     // capture log level or fall back to info
     let level_filter = std::env::var("RUST_LOG")
         .unwrap_or(format!("{:?}", crate::globals::get_loglevel()))
