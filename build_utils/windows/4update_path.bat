9a3a3fb71e82a497d5709c2c6e1d278d03080dc9
GamingGuy003 <bjornfelixh@gmail.com>
2025-03-22 14:14:12 +0100

    add cargo to path

--- build_utils/windows/4update_path.bat
+++ build_utils/windows/4update_path.bat
@@ -1,1 +1,1 @@
-setx Path "%PATH%;C:\msys64\mingw64\include;C:\msys64\mingw64\bin;C:\msys64\mingw64\lib"
+setx Path "%PATH%;C:\msys64\mingw64\include;C:\msys64\mingw64\bin;C:\msys64\mingw64\lib;%%USERPROFILE%%\.cargo\bin"
